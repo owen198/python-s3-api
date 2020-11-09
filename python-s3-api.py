27ca436b47d30b9841895e2ed77b900b48a27d82
owen198 <cfleu198@gmail.com>
2020-11-09 13:02:06 +0800

    transfer

--- python-s3-api.py
+++ python-s3-api.py
@@ -68,7 +68,7 @@ def get_content():
     #if SPECIFIC_TIME.isdigit() and len(SPECIFIC_TIME) > 3:
     #    print('user specified time and date')
     TS = datetime.datetime.fromtimestamp(int(SPECIFIC_TIME[0:10]))
-    #TS = TS + datetime.timedelta(hours=8)
+    TS = TS + datetime.timedelta(hours=8)
     #    print('Feature assorcated timestamp in Query Date=', TS)
     #else:
         #Dr. Ho: we use specified time instead of search range
@@ -94,9 +94,6 @@ def get_content():
     #print ('Raw Data-' + DEVICE_NAME + '-' + HOUR + '-'+ MIN + '-' + SECOND + '_25600.bin')
     FILE_NAME = 'Raw Data-' + DEVICE_NAME + '-' + HOUR + '-'+ MIN + '-' + SECOND + '_25600.bin'
 
-
-    '#1HSM/ROT/vPodPRO/#1內冷式ROT Roller WS_vpod/2020/08/01/'
-    'Raw Data-#1內冷式ROT Roller WS_vpod-00-56-26_25600.bin'
 
     #FILE_NAME = query_file (TS, S3_BUCKET, PATH_DEST, EQU_ID)
     #FILE_NAME = 'Raw Data-#1內冷式ROT Roller WS_vpod-00-56-28_25600.bin'
