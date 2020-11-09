a7fc6138312ced60768920fe9b529119934784b0
owen198 <cfleu198@gmail.com>
2020-11-09 10:48:21 +0800

    transfer

--- python-s3-api.py
+++ python-s3-api.py
@@ -96,7 +96,7 @@ def get_content():
 
     #FILE_NAME = query_file (TS, S3_BUCKET, PATH_DEST, EQU_ID)
     #FILE_NAME = 'Raw Data-#1內冷式ROT Roller WS_vpod-00-56-28_25600.bin'
-                 'Raw Data-#1內冷式ROT Roller WS_vpod-08-56-26_25600.bin'
+
     print("FILE_NAME:",FILE_NAME)
     FILE_NAME=FILE_NAME.encode('utf-8').strip()
     print("FILE_NAME:",FILE_NAME)    
