2d23b5af10ebabf9efb41dfe78870e9728f13f40
owen198 <cfleu198@gmail.com>
2020-11-10 13:40:11 +0800

    put todo for merge

--- python-s3-api.py
+++ python-s3-api.py
@@ -46,7 +46,7 @@ def get_content():
     target_obj = jsonobj['targets'][0]['target']
     date_obj = jsonobj['range']['from']
     DATE = datetime.datetime.strptime(date_obj, '%Y-%m-%dT%H:%M:%S.%fZ')
-    DATE = DATE + datetime.timedelta(hours=8)
+    DATE = DATE + datetime.timedelta(hours=16)
     # DATE = DATE + datetime.timedelta(hours=8) - datetime.timedelta(days=1)    
     DATE = DATE.strftime('%Y-%m-%d')
 
